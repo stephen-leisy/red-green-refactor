2494a9231a5fa248f68edf8d31fe88fc55c36b17
Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>
2021-03-23 09:09:26 -0700

    fixed expect any to follow munge format instead of just expecting object.

--- fetch-quotes.test.js
+++ fetch-quotes.test.js
@@ -3,6 +3,10 @@ const getQuote = require('./fetch-quotes.js');
 describe('fetch quotes', () => {
   it('it should hit an API to return a single formatted quote', async () => {
     const quote = await getQuote();
-    expect(quote).toEqual(expect.any(Object));
+    expect(quote).toEqual({
+      name: expect.any(String),
+      text: expect.any(String),
+      image: expect.any(String),
+    });
   });
 });
